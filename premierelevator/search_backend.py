70931a599a93f2389477d1354721024098ac21dd
K Md Mamunur Rashid <mamun1980@gmail.com>
2015-01-03 03:53:30 +0600

    update view

--- premierelevator/search_backend.py
+++ premierelevator/search_backend.py
@@ -3,15 +3,22 @@ from haystack.backends.elasticsearch_backend import ElasticsearchSearchEngine
 
 class FuzzyBackend(ElasticsearchSearchBackend):
     def build_search_kwargs(self, query_string, **kwargs):
-        kwargs = {
-            'query': {
-                'fuzzy_like_this': {
-                    'like_text': query_string,
-                    'prefix_length': 0
+        if query_string == '*:*':
+            kwargs = {
+                'query': {
+                    "match_all": {}
                 },
-            },
+            }
+        else:
+            kwargs = {
+                'query': {
+                    'fuzzy_like_this': {
+                        'like_text': query_string,
+                        'prefix_length': 1
+                    },
+                },
 
-        }
+            }
         return kwargs
 
 
